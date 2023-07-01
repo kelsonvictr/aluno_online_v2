52c0560af8cb518d8c0d1286ee9cd00debc8492d
rfujioka <rodrigofujioka@gmail.com>
2023-07-01 12:45:28 -0300

    Inclusão de H2 para ser utilizado pelos alunos e da classe repository com as consultas voltadas para alunos

--- src/main/java/com/alunoonline/api/repository/AlunoRepository.java
+++ src/main/java/com/alunoonline/api/repository/AlunoRepository.java
@@ -2,8 +2,23 @@ package com.alunoonline.api.repository;
 
 import com.alunoonline.api.model.Aluno;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface AlunoRepository extends JpaRepository<Aluno, Long> {
+
+    public List<Aluno> findAlunoByNome(String nome);
+
+    public Aluno findAlunoByEmail(String email);
+    public List<Aluno>
+    findAlunoByNomeAndEmail(String nome, String email);
+
+    public List<Aluno> findAllByNomeOrderByNomeAsc(String nome);
+
+    @Query("select u from Aluno u where u.nome=:nome")
+    public List<Aluno> buscarPorNome(@Param("nome") String nome);
 }
