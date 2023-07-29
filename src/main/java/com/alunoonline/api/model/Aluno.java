d7c3910e8b1904038acd99fb14f93c95783bb979
rfujioka <rodrigofujioka@gmail.com>
2023-07-29 13:29:48 -0300

    INcluindo testes unitários de exemplo para turma de BackEnd JR

--- src/main/java/com/alunoonline/api/model/Aluno.java
+++ src/main/java/com/alunoonline/api/model/Aluno.java
@@ -8,6 +8,7 @@ import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -16,6 +17,7 @@ import java.util.List;
 
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 @Data
 @Entity
 public class Aluno implements Serializable {
--- src/main/java/com/alunoonline/api/model/dtos/AlunoDTO.java
+++ src/main/java/com/alunoonline/api/model/dtos/AlunoDTO.java
@@ -2,9 +2,11 @@ package com.alunoonline.api.model.dtos;
 
 import com.alunoonline.api.validator.EmailValidation;
 import com.alunoonline.api.validator.NomeValidation;
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class AlunoDTO {
     private Long id;
 
